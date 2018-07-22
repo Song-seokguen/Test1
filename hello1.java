805ee28d0999e6d4042a4d2baab39b54c7eb2f67
sekgun16@gmail.com <sekgun16@gmail.com>
2018-07-22 16:20:08 +0900

    hello1.java

--- hello1.java
+++ hello1.java
@@ -5,6 +5,12 @@ public class hello1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world...");
+		for(int i=0; i<10; i++) {
+			System.out.println("hello "+i);
+		}
+		
+		//20180722
+		System.out.println("end of Doc");
 	}
 
 }
